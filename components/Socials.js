8f2d9a8d284916556c573704f444984f3f4468d7
Md Sabbir Ahamed <redoyahamedrs@gmail.com>
2025-07-05 20:57:26 +0600

    social icon and link added

--- components/Socials.js
+++ components/Socials.js
@@ -2,24 +2,30 @@
 import Link from "next/link";
 
 // icons
-import {RiLinkedinLine,RiGithubLine,RiFacebookLine,RiInstagramLine} from "react-icons/ri";
+import {RiLinkedinLine,RiGithubLine,RiFacebookLine,RiInstagramLine,RiYoutubeLine,RiMailLine,RiOrcid} from "react-icons/ri";
 
 const Socials = () => {
-  return <div href={''} className="flex items-center gap-x-5 text-lg">
-    <Link href={''} className="hover:text-accent transition-all duration-300">
+  return <div href={''} className="flex items-center gap-x-5 text-2xl">
+    <Link href={'https://github.com/Redoy0'} target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
       <RiGithubLine/>
     </Link>
     
-    <Link href={''} className="hover:text-accent transition-all duration-300">
+    <Link href={''} target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
       <RiLinkedinLine/>
     </Link>
     
-    <Link href={''} className="hover:text-accent transition-all duration-300">
+    <Link href={''} target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
       <RiFacebookLine/>
     </Link>
     
-    <Link href={''} className="hover:text-accent transition-all duration-300">
+    <Link href={''} target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
       <RiInstagramLine/>
+    </Link>
+    <Link href={''} target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine/>
+    </Link>
+    <Link href={''} target="_blank" rel="noopener noreferrer" className="hover:text-accent transition-all duration-300">
+      <RiMailLine/>
     </Link>
 
   </div>;
