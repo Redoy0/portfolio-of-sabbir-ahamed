e9b69025add96f2bbb5f7468153dc77c6f20eb31
Md Sabbir Ahamed <redoyahamedrs@gmail.com>
2025-09-02 16:39:49 +0600

    avater changed

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,14 +4,14 @@ import Image from "next/image";
 const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
-    <Image
-    src={'/avatar.png'}
-    width={737}
-    height={678}
-    alt=''
-    className='translate-z-0 w-full h-full'/>
+      <Image
+        src={'/avatar/luffy2_1.png'}
+        width={737}
+        height={678}
+        alt=''
+        className='translate-z-0 w-full h-full' />
     </div>
-    );
+  );
 };
 
 export default Avatar;
